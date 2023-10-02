6491e04a06ea4decf2c331dd921a5f84580e4c1c
nhurtadoc1 <55059817+nhurtadoc1@users.noreply.github.com>
2023-10-02 19:53:04 +0200

    Update Ejercicio1.ts
    Updated Ejercicio1.ts to be recursive

--- Ejercicio1.ts
+++ Ejercicio1.ts
@@ -1,43 +1,48 @@
-// Function that bubble sorts an array. If outputIteration is set to true, it'll output in console how many iterations it took to sort the array
-function BubbleSort(input:Array<number>, outputIteration?:boolean):Array<number> {
+// Recursive function that bubble sorts an array. If outputIteration is set to 0, it'll output in console how many iterations it took to sort the array
+function BubbleSortRec(input:Array<number>, outputIteration?:number):Array<number> {
     let output: Array<number> = [...input]
+    let changes = 0;
     let itLength = output.length - 1;
-    for(let i = 0; i < output.length - 1; i++) {
-        let changes = 0;
-        for(let j = 0; j < output.length - 1; j++) {
-            if(output[j] > output[j+1]) {
-                let temp = output[j];
-                output[j] = output[j+1];
-                output[j+1] = temp;
-                changes++;
-            }
-        }
-        if(changes === 0) {
-            if(outputIteration === true) {
-                console.log("Exited at iteration " + i + "/" + itLength);
-            }
-            return output;
+    for(let i = 0; i < itLength; i++) {
+        if(output[i] > output[i+1]) {
+            let temp = output[i];
+            output[i] = output[i+1];
+            output[i+1] = temp;
+            changes++;
         }
     }
-    if(outputIteration === true) {
-        console.log("Exited at iteration " + itLength + "/" + itLength);
+    if(changes === 0) {
+        if(typeof outputIteration !== "undefined") {
+            console.log("Exited at iteration " + outputIteration + "/" + itLength);
+        }
+        return output;
+    } else {
+        if(typeof outputIteration === "undefined") {
+            return BubbleSortRec(output);
+        } else {
+            return BubbleSortRec(output, outputIteration+1);
+        }
     }
-    return output;
 }
+
+
 
 const randomArray: number[] = [7, 4, 2, 9, 6, 3, 5, 1, 10, 8];
 const nearSortedArray: number[] = [1, 3, 2, 4, 5, 6, 7, 8, 9, 10];
 const worstArray: number[] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const bestArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(randomArray);
-const orderedArray1 = BubbleSort(randomArray, true);
+const orderedArray1 = BubbleSortRec(randomArray, 0);
 console.log(orderedArray1);
 console.log("----------");
 console.log(nearSortedArray);
-const orderedArray2 = BubbleSort(nearSortedArray, false);
+const orderedArray2 = BubbleSortRec(nearSortedArray);
 console.log(orderedArray2);
 console.log("----------");
 console.log(worstArray);
-const orderedArray3 = BubbleSort(worstArray);
+const orderedArray3 = BubbleSortRec(worstArray, 0);
 console.log(orderedArray3);
 console.log("----------");
-console.log(randomArray);
+console.log(bestArray);
+const orderedArray4 = BubbleSortRec(bestArray, 0);
+console.log(orderedArray4);
